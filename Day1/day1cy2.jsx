937c9671600277282c52b2a53c59e7a03f530cc0
Priyadharshini174 <727723eucs174@skcet.ac.in>
2024-08-05 21:01:21 +0530

    Add files via upload

---
+++ Day1/day1cy2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Add()
+{
+   
+    direction = directions.map(direction => direction);
+    document.write(directions:String[]);
+    return(
+    <div>
+         <button onClick={() => handleClick()}>Submit</button>
+    </div>
+    );
+    };
+export default Add;
