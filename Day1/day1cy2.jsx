810632e6ef825559c257747281bfeffd51b2f6fc
Priyadharshini174 <727723eucs174@skcet.ac.in>
2024-08-11 20:12:01 +0530

    Update day1cy2.jsx

--- Day1/day1cy2.jsx
+++ Day1/day1cy2.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-
-function Add()
-{
-   
-    direction = directions.map(direction => direction);
-    document.write(directions:String[]);
-    return(
-    <div>
-         <button onClick={() => handleClick()}>Submit</button>
-    </div>
-    );
-    };
-export default Add;
+     let directions = ["North", "South", "East", "West"];
+    function ref()
+    {
+        document.write(directions);
+    }
+    ref()
+    function Add() {
+      directions = directions.map(direction => direction + " News");
+      document.write(directions);
+      
+      <div>
+        <button onClick={ref}>ref</button>
+      </div>
+    }
+    function D()
+    {
+        return(
+            <div  className="arrayDisplay">
+                <button onClick={Add}>Add</button>
+                
+            </div>
+        )
+    }export default D;
